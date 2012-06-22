8a624e6c66866d239f096c02a9f5735721861c84
Caesar Chi <clonncd@gmail.com>
2012-06-22 09:16:27 +0800

    remove app.js remove other route setting.

--- app.js
+++ app.js
@@ -36,10 +36,6 @@ app.configure( 'production', function (){
 
 // Routes
 app.get( '/', routes.index );
-app.post( '/create', routes.create );
-app.get( '/destroy/:id', routes.destroy );
-app.get( '/edit/:id', routes.edit );
-app.post( '/update/:id', routes.update );
 
 app.listen( 3001, '127.0.0.1', function (){
   console.log( 'Express server listening on port %d in %s mode', app.address().port, app.settings.env );
