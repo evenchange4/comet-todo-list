7ddc1e71ea9b24fd49e0c048ee7bff8c78648abf
Caesar Chi <clonncd@gmail.com>
2012-06-25 02:48:11 +0800

    change room authorize, and user can open a new todo room.

--- routes/index.js
+++ routes/index.js
@@ -1,26 +1,73 @@
 var mongoose = require( 'mongoose' );
+var utils    = require('connect').utils;
 var Todo     = mongoose.model( 'Todo' );
+var Hashlist = mongoose.model( 'Hashlist' );
 var utils    = require( 'connect' ).utils;
 
 exports.index = function ( req, res, next ){
+  var lang = {
+        create: "Create"
+      };
+  res.render( 'index', {
+      title : 'Comet Todo list',
+      lang : lang
+  });
+};
+
+exports.room = function ( req, res, next ){
+  if (! req.params.hasOwnProperty("room")) {
+    res.redirect("/");
+  }
+
   Todo.
-    find({ user_id : req.cookies.user_id }).
+    find({ room_id : req.params.room }).
     sort( 'updated_at', -1 ).
     run( function ( err, todos ){
       if( err ) return next( err );
 
-      res.render( 'index', {
-          title : 'Express Todo Example',
+      res.render( 'room', {
+          title : 'Comet Todo list',
+          id: req.params.room,
           todos : todos
       });
     });
 };
 
-exports.current_user = function ( req, res, next ){
-  if( !req.cookies.user_id ){
-    //res.cookie( 'user_id', utils.uid( 32 ));
-    res.cookie( 'user_id', "123456789");
-  }
+exports.create = function ( req, res, next ){
+  res.clearCookie('room_id');
+  var uid = utils.uid(10);
 
-  next();
+  new Hashlist({
+    hash_id    : uid,
+    enable     : true,
+    updated_at : Date.now()
+  }).save( function ( err, list, count ){
+    if( err ) res.redirect('/');
+
+    res.redirect('/room/' + uid);
+  });
+  
+};
+
+exports.current_room = function ( req, res, next ){
+  if (!req.params.hasOwnProperty('room')) {
+    res.render('404', {title: 'Error 404'});
+    return;
+  }
+  var uid = req.params.room;
+
+  Hashlist.find({
+      hash_id: uid       
+  }).
+  run(function (err, list) {
+    if (err) {
+      res.render('404', {title: 'Error 404'});
+      return;
+    }
+    
+    res.cookie( 'room_id', uid, {path: '/'});
+    next();
+          
+  });
+
 };
