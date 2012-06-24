d01b40411aeacada54fd3ad7bfb38d58af2e2231
Caesar Chi <clonncd@gmail.com>
2012-06-25 02:47:08 +0800

    update db.js, updat a hash list table on db.

--- db.js
+++ db.js
@@ -2,11 +2,19 @@ var mongoose = require( 'mongoose' );
 var Schema   = mongoose.Schema;
 
 var Todo = new Schema({
-    user_id    : String,
+    room_id    : String,
     content    : String,
     updated_at : Date
 });
 
-mongoose.model( 'Todo', Todo );
+var Hashlist = new Schema({
+    hash_id    : String,
+    enable     : Boolean,
+    updated_at : Date
+});
 
+mongoose.model( 'Todo', Todo );
+mongoose.model( 'Hashlist', Hashlist);
+
+//mongoose.connect( 'mongodb://clonn:1234@flame.mongohq.com:27061/todoclonn' );
 mongoose.connect( 'mongodb://localhost/express-todo' );
